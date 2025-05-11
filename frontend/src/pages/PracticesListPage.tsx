410ba87b40f8ab9acbd3318a553e63834a375f08
stas_ <dromanqs@mail.ru>
2025-05-11 17:20:09 +0500

    PracticesListPage.tsx

--- frontend/src/pages/PracticesListPage.tsx
+++ frontend/src/pages/PracticesListPage.tsx
@@ -16,7 +16,6 @@ export function PracticesListPage() {
 
   return (
     <div>
-      <Header />
       <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
         <h1 className="text-3xl font-bold mb-8">Практические задания</h1>
 
