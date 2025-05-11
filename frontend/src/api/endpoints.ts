873a35d8c4e2f754fd5b605c81e0743bef6c1815
stas_ <dromanqs@mail.ru>
2025-05-11 17:10:47 +0500

    const апи юрл

--- frontend/src/api/endpoints.ts
+++ frontend/src/api/endpoints.ts
@@ -3,11 +3,10 @@ export const API_BASE_URL = 'http://127.0.0.1:8000';
 import { authService } from '../services/auth';
 
 export const ENDPOINTS = {
-  courses: `${API_BASE_URL}/courses/`,
-  questions: (courseId: number, practiceId: number) => `${API_BASE_URL}/courses/${courseId}/practice/${practiceId}/question`,
+  courses: `${API_BASE_URL}/courses/False/`,
+  questions: (courseId: number, practiceId: number) => `${API_BASE_URL}/courses/False/${courseId}/practice/${practiceId}/question`,
   answers: (courseId: number, practiceId: number, questionId: number) =>
-    `${API_BASE_URL}/courses/${courseId}/practice/${practiceId}/question/${questionId}/answers`,
-  userProgress: `${API_BASE_URL}/user-progress/`,
+    `${API_BASE_URL}/courses/False/${courseId}/practice/${practiceId}/question/${questionId}/answers`,
 } as const;
 
 export const getHeaders = () => {
