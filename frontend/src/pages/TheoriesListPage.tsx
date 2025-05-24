35e5f12b9cff01b5555dee899be1d6ae43905f8b
stas_ <dromanqs@mail.ru>
2025-05-24 21:08:06 +0500

    Кнопка выхода назад, Изменение интерфейса теории

--- frontend/src/pages/TheoriesListPage.tsx
+++ frontend/src/pages/TheoriesListPage.tsx
@@ -4,7 +4,7 @@ import { Header } from '../components/Header';
 import { LoadingSpinner } from '../components/LoadingSpinner';
 import { ErrorMessage } from '../components/ErrorMessage';
 import { useTheories } from '../hooks/useTheories';
-import { Brain, Lock, CheckCircle } from 'lucide-react';
+import { Brain, Lock, CheckCircle, ArrowLeft } from 'lucide-react';
 
 export function TheoriesListPage() {
   const { id } = useParams();
@@ -28,9 +28,16 @@ export function TheoriesListPage() {
   return (
     <div className="bg-gray-100 dark:bg-gray-900">
       <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
-        <h1 className="text-3xl font-bold mb-8">Теоретические материалы</h1>
+        <div className="flex justify-between">
+            <h1 className="text-3xl font-bold mb-8">Теоретические материалы</h1>
+            <button className="flex items-center"
+              onClick={() => navigate('/')}>
+              <ArrowLeft className="w-5 h-5" />
+              Назад
+            </button>
+        </div>
 
-        <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
+        <div className="grid gap-10 md:grid-cols-2 lg:grid-cols-4">
           {theories.map((theory) => {
               const isRead = readParts.includes(theory.theory_id);
               const accessible = isPartAccessible(theory.theory_id);
@@ -58,7 +65,6 @@ export function TheoriesListPage() {
                     {!accessible && (
                       <div className="absolute inset-0 backdrop-blur-md bg-black/30 flex items-center justify-center">
                         <Lock className="w-40 h-40 text-white/80" />
-                        <p className="">Прочтите предыдущую теорию</p>
                       </div>
                     )}
 
@@ -66,7 +72,7 @@ export function TheoriesListPage() {
                     <div className="absolute inset-0 p-6 flex flex-col justify-between">
                       <div className="flex justify-between items-start">
                         <h2 className="text-xl font-bold text-white">
-                          Part {theory.theory_id}
+                          Урок {theory.theory_id}
                         </h2>
                         {isRead && (
                           <div className="bg-green-500/90 rounded-full p-1">
@@ -76,13 +82,18 @@ export function TheoriesListPage() {
                       </div>
 
                       <div>
-                        <h3 className="text-lg font-medium text-white mb-2">
+                        {accessible && (
+                            <h3 className="text-lg font-medium text-white mb-2">
                           {theory.title_theory}
                         </h3>
+                        )}
                         {accessible && (
                           <p className="text-gray-200 text-sm line-clamp-3">
                             {theory.text_theory}
                           </p>
+                        )}
+                        {!accessible && (
+                          <p className="text-gray-200 flex justify-center text-sm line-clamp-3">Прочтите предыдущую теорию</p>
                         )}
                       </div>
                     </div>
