3fa69675af219bd0f1135fa168dbc07da7fedbcc
stas_ <dromanqs@mail.ru>
2025-08-29 20:31:20 +0500

    Frontend - pages - PracticePage - 0

--- frontend/src/pages/PracticePage.tsx
+++ frontend/src/pages/PracticePage.tsx
@@ -30,7 +30,6 @@ export function PracticePage() {
 
   return (
     <div>
-      <Header />
       <main className="max-w-3xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
         <h1 className="text-3xl font-bold mb-8">Практика</h1>
         <div className="mb-4">
