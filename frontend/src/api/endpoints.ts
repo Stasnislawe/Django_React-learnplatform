48e89e7469e6be7f4c062d65aef2840fe7ec9d67
stas_ <dromanqs@mail.ru>
2025-08-29 20:24:37 +0500

    Frontend - api - endpoints изменил ссылки под юрл

--- frontend/src/api/endpoints.ts
+++ frontend/src/api/endpoints.ts
@@ -1,8 +1,10 @@
 export const API_BASE_URL = 'http://127.0.0.1:8000';
 
 export const ENDPOINTS = {
-  courses: `${API_BASE_URL}/courses/False/`,
-  questions: (courseId: number, practiceId: number) => `${API_BASE_URL}/courses/False/${courseId}/practice/${practiceId}/question`,
-  answers: (courseId: number, practiceId: number, questionId: number) =>
-    `${API_BASE_URL}/courses/False/${courseId}/practice/${practiceId}/question/${questionId}/answers`,
+  courses: (free: string) => `${API_BASE_URL}/courses/${free}/`,
+  practices: (free: string, courseId: number) => `${API_BASE_URL}/courses/${free}/${courseId}/practice/`,
+  questions: (free: string, courseId: number, practiceId: number) =>
+    `${API_BASE_URL}/courses/${free}/${courseId}/practice/${practiceId}/question`,
+  answers: (free: string, courseId: number, practiceId: number, questionId: number) =>
+    `${API_BASE_URL}/courses/${free}/${courseId}/practice/${practiceId}/question/${questionId}/answers`,
 } as const;
